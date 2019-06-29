a70f58bd41f4413cf0a9b0e6753b569a9e599afb
Samuel Christopher <Ebusameric@gmail.com>
2019-06-29 08:51:14 +0100

    add more colors to skills

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
+const stacks = (stac) => {
+	return {
+		'react': { name: 'ReactJs', background: '#218e94' },
+		'html5': { name: 'HTML5', background: '#ca2b03' },
+		'javascript': { name: 'JavaScript', background: '#cea11a' },
+		'd3': { name: 'D3', background: '#f5824c' },
+		'jquery': { name: 'jQuery', background: '#172c45' },
+		'sass': { name: 'Sass', background: '#b55f8c' },
+		'express': { name: 'Expressjs', background: '#888585' },
+		'mongodb': { name: 'MongoDB', background: '#4aad3a' },
+		'boostrap': { name: 'Bootstrap', background: '#5f3f88' },
+		'css3': { name: 'CSS3', background: '#379ad6' }
+	}[stac];
+}
+
 const EachProject = (props) => {
 	let Descript;
 	let dateStr;
 	let descripArr = props.project.description;
+	let Skills;
 
 	if(descripArr) {
 		Descript = descripArr.split('.').map( a => <p>{a}.</p>);
 		dateStr = new Date(props.project.create_on).toDateString();
+		Skills = props.project.skills.map((a, i) => <span style={{background: '#ca2b03', top: i * 30}}>{a}</span>)
 	}
 	return (
 		<div className="contents" style={{ backgroundColor:props.backgr }}>
 			<div className="each-project">
 				<div className="project-img">
+					{ Skills }
 					<img src={props.project.img_url} alt="" />
 				</div>
 				<div className="project-info">
@@ -70,7 +88,7 @@ const Projects = (props) => {
 				<div id="all-project" className="first">
 						{	props.project.map((ele, i) => {
 								if(i % 2 === 0)return <EachProject project={ele} backgr={'rgb(43, 43, 43)'} />;
-								return <EachProject project={ ele }/> 
+								return <EachProject project={ ele } index={i}/> 
 							}) 
 						}
 				</div>
