f41a70b3f68075b6cfcdaa19d051cfa4a166f862
Samuel Christopher <Ebusameric@gmail.com>
2019-07-01 15:25:51 +0100

    Updates

---
+++ precache-manifest.e2b18a3a7a66897db201ba1ec96f49b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd4e2593b50a58c32edc1fc92d780e2",
+    "url": "/sammychris.github.io/index.html"
+  },
+  {
+    "revision": "77b18ca8a59ec06b76e0",
+    "url": "/sammychris.github.io/static/css/main.c04ef3c9.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/sammychris.github.io/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "77b18ca8a59ec06b76e0",
+    "url": "/sammychris.github.io/static/js/main.4cf58904.chunk.js"
+  },
+  {
+    "revision": "18b65b232e4cdb601712",
+    "url": "/sammychris.github.io/static/js/runtime~main.061a9f35.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b73845ef7d7185e83788be9534e1535b.js"
+  "/sammychris.github.io/precache-manifest.e2b18a3a7a66897db201ba1ec96f49b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sammychris.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
