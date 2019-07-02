cb7e003b99a86f27ba27fa42bc9dc4846154a974
Samuel Christopher <Ebusameric@gmail.com>
2019-07-02 16:49:57 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sammychris.github.io/precache-manifest.e2b18a3a7a66897db201ba1ec96f49b9.js"
+  "/sammychris.github.io/precache-manifest.5177eedd8f5c4dcae7d675afb4c2c522.js"
 );
 
 self.addEventListener('message', (event) => {
